c5586182fe086f17514836de056390ee14908d71
Peter Shipley <peter.shipley@gmail.com>
2022-07-04 22:57:53 -0700

    clean up

--- generate_sub_cmd.py
+++ generate_sub_cmd.py
@@ -45,7 +45,8 @@ Preset: FuriHalSubGhzPreset{modu}{srate}Async
 Protocol: RAW
 """
 
-    print(zerolen, onelen, baudrate)
+    if _verbose:
+        print(f"zerolen={zerolen}, onelen={onelen}, baudrate={baudrate}")
 
     if baudrate is not None:
         zerolen = onelen = (1/baudrate) * 1000000
@@ -57,7 +58,9 @@ Protocol: RAW
     zerolen=int(zerolen)
     onelen=int(onelen)
 
-    print(zerolen, onelen, baudrate, zerolen_off, onelen_off)
+    if _verbose:
+        print( f"zerolen={zerolen}, onelen={onelen}, baudrate={baudrate}, "
+              f"zerolen_off={zerolen_off:0.04f}, onelen_off={onelen_off:0.03f}" )
 
     if pause == 0:
         # Pause must be non-zero.
@@ -99,7 +102,8 @@ Protocol: RAW
         datalines.append(f'RAW_Data: {" ".join(batch)}')
     res += '\n'.join(datalines)
 
-    print(f"delta_off {delta_off}")
+    if _verbose:
+        print(f"delta_off {delta_off}")
 
     return res
 
@@ -132,6 +136,7 @@ Modulation_Presets = {
 def arg_line():
     # if len(sys.argv) > 1:
     #     main_default()
+    global _verbose
 
     epilog  = f'''
     example:\n\t {sys.argv[0]} -f 302500000 -0 333 -1 333 -m -B 0110100001000
@@ -151,6 +156,10 @@ def arg_line():
         formatter_class=argparse.RawDescriptionHelpFormatter
     )
 
+    parser.add_argument('-v', '--verbose', dest="verb",
+                        default=0,
+                        help='Increase debug verbosity', action='count')
+
     parser.add_argument("-o", "--outfile", dest="out_file",
                         default="subghz.sub",
                         help="Output filename")
@@ -169,6 +178,10 @@ def arg_line():
                         type=int,
                         default=None,
                         help="Length of mark bit in ms")
+
+    parser.add_argument("-i", "--invert", dest="invert",
+                        action='store_true',
+                        help="Invert bits")
 
     parser.add_argument("-b", "--baud", dest="baud_rate",
                         type=int,
@@ -205,10 +218,19 @@ def arg_line():
                         type=int, default=1,
                         help="delay padding between repeated sequences")
 
-    args, unknown_args = parser.parse_known_args()
+    args_data, unknown_args = parser.parse_known_args()
 
-    print(f"\nargs: {args}\n")
+    if args_data.verb:
+        _verbose += args_data.verb
 
+    if _verbose:
+        print(f"\nargs: {args_data}\n")
+
+    return args_data
+
+def gen_data():
+
+    args = arg_line()
     zero_len = one_len = None
 
     if args.baud_rate is None:
@@ -228,31 +250,35 @@ def arg_line():
 
     if args.hex_data:
         bin_data = hex2bin(args.hex_data)
-        print(f"hex_data: {args.hex_data}")
+        if _verbose:
+            print(f"hex_data: {args.hex_data}")
     else:
         bin_data = args.bin_data
 
-
-    print(f"bin_data: {bin_data}")
+    # print(f"bin_data: {bin_data}")
 
     if args.manch_encode:
         bin_data = ''.join(['10' if b == '1' else '01' for b in bin_data])
 
-    print(f"bin_data: {bin_data}")
+    if args.invert:
+        bin_data = ''.join(['0' if b == '1' else '1' for b in bin_data])
 
-    # if args.verb:
-    #    verbose = args.verb
+    if _verbose:
+        print(f"bin_data: {bin_data}")
+
 
     packet_data = gen_sub(args.send_freq,
                     zero_len, one_len, args.baud_rate, 0,
                     bin_data,
                     mod_settings[0], mod_settings[1])
 
-    print(f"packet_data: {packet_data}")
-    #with open(out_file, 'w', encoding="utf-8") as fd:
-    #    print(packet_data, file=fd)
+    if _verbose:
+        print(f"packet_data: {packet_data}")
+
+    with open(args.out_file, 'w', encoding="utf-8") as fd:
+        print(packet_data, file=fd)
 
     sys.exit()
 
 if __name__ == '__main__':
-    arg_line()
+    gen_data()
